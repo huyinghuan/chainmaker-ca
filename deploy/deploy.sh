c62493d9a69589dabb28a1a4495e64bf094cb03e
liuxinfeng <liuxf@yunphant.com>
2021-02-23 13:58:41 +0800

    deploy

---
+++ deploy/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+path=`pwd`
+docker run -d \
+-p 8090:8090 \
+-w /chainmaker-ca \
+-v $path/chainmaker-ca:/chainmaker-ca \
+-v $path/log:/ca-log \
+--name chainmaker-ca \
+--restart always \
+ubuntu:18.04 \
+bash -c "./chainmaker-ca -config ./"
